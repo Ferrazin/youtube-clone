5c47170f9c86663ae3b12d012b103610732cac20
Gregorio Ferrazin <gregorio.ferrazin@gmail.com>
2018-12-08 17:43:43 +0100

    search works, needs styling

--- src/index.js
+++ src/index.js
@@ -2,36 +2,41 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/searchbar";
-import VideoList from './components/videoList'
+import VideoList from "./components/videoList";
 import VideoDetail from "./components/videoDetail";
-import './style/style.css'
+import "./style/style.css";
 const API_KEY = "AIzaSyCiq6KWB9JfKvabI3M6HDMUqLMLcsSxlt8";
-
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
-        videos: [],
-        selectedVideo: null,
-     };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: "savannah cats" }, videos => {
-      this.setState({ 
-          videos: videos,
-          selectedVideo: videos[0]
-        });
-    });
+    this.videoSearch('surf')
   }
+  videoSearch = term => {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  };
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList 
-        onVideoSelect={selectedVideo => {this.setState({selectedVideo})}}
-        videos={this.state.videos}  />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => {
+            this.setState({ selectedVideo });
+          }}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
